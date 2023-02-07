16136cccf022fff188e60db2ce5996662902ab41
Fabian Huegle <fabian.huegle@stryker.com>
2023-02-07 16:52:02 +0100

    [AnimationTest] Added project

---
+++ AvaloniaAnimationTest/MainWindow.axaml.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Rendering;
+
+namespace AvaloniaAnimationTest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnChecked(object sender, RoutedEventArgs e)
+        {
+            if (!_useRenderTransformCheckBox.IsChecked.Value)
+            {
+                _animGrid.Width = 600;
+            }
+            else
+            {
+                _animGrid.Classes.Add("large");
+            }
+        }
+
+        private void OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            if (!_useRenderTransformCheckBox.IsChecked.Value)
+            {
+                _animGrid.Width = 200;
+            }
+            else
+            {
+                _animGrid.Classes.Remove("large");
+            }
+        }
+    }
+}
